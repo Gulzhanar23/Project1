07417323767565b159e22562e67eae1746d4424d
Gulzhanar23 <berikgulzhanar@gmail.com>
2023-01-23 18:37:56 -0500

    V2

--- src/Task11.java
+++ src/Task11.java
@@ -3,14 +3,14 @@ public class Task11 {
         String[][] marvelHeroes={{"Captain America","Spider-man","Hulk","Hulk","Ant-man","Ironman","Thor"},
                                  {"Thor","Spider-man","Groot","Groot"}
         };
-        String duplicate=marvelHeroes[0][0];
-        String duplicate2;
+        String origin=marvelHeroes[0][0];
+        String duplicate;
         for(int i=0;i< marvelHeroes.length;i++){
             for (int j = 1; j < marvelHeroes[i].length; j++) {
-                duplicate2=duplicate;
-                duplicate=marvelHeroes[i][j];
-                if(duplicate==duplicate2){
-                    System.out.println(marvelHeroes[i][j]+" is a duplicate Element");
+                duplicate=origin;
+                origin=marvelHeroes[i][j];
+                if(origin==duplicate){
+                    System.out.println(marvelHeroes[i][j]+" is a duplicate");
                 }
             }
         }
